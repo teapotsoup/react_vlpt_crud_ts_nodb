c9dabf16199cc91730243eb9d45bce548060f8a2
teapotsoup <0902ab@naver.com>
2022-07-16 22:28:30 +0900

    UI complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+import TodoCreate from "./components/TodoCreate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoTemplate from "./components/TodoTemplate";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+
 function App() {
   return (
-    <div className="App">
-        <h1>clear</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate/>
+      </TodoTemplate>
+    </>
   );
 }
 
